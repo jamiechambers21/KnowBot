3cf937869058f3b644dd3eb81c525f1b0e5ec819
Jamie Chambers <jamiechambers21@gmail.com>
2024-10-09 21:06:32 +0200

    PDF reader added

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+def get_pdf_text(pdf_docs):
+    print("get_pdf_text: START")
+    text = ""
+    for pdf in pdf_docs:
+        # Initilize PDF Reader Object
+        pdf_reader = PdfReader(pdf)
+        # For each page, add text
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    print("get_pdf_text: END")
+    return text
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title="Knowbot", page_icon="🧿")
 
     st.header("Knowbot - Ask me, I'll know")
@@ -8,8 +23,16 @@ def main():
 
     with st.sidebar:
         st.subheader("Your files")
-        st.file_uploader("Upload PDFs here")
-        st.button("Process")
+        pdf_docs = st.file_uploader("Upload PDFs here", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # Get PDF text
+                raw_text = get_pdf_text(pdf_docs)
+                st.write(raw_text)
+
+                # Get Text Chunks
+
+                # Create vector store
 
 if __name__ == '__main__':
     main()
