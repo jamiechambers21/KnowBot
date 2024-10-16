090ea6c6c657fbb367217cf18afb80b13db3ee16
Jamie Chambers <jamiechambers21@gmail.com>
2024-10-16 20:36:00 +0200

    Update imports

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstore import FAISS
 
 def get_pdf_text(pdf_docs):
     print("get_pdf_text: START")
@@ -13,6 +16,30 @@ def get_pdf_text(pdf_docs):
             text += page.extract_text()
     print("get_pdf_text: END")
     return text
+
+def get_text_chunks(text):
+    print("get_text_chunks: START")
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+
+    chunks = text_splitter.split_text(text)
+    print("get_text_chunks: END")
+    return chunks
+
+def get_vectorstore(text_chunks):
+    print("get_vectorstore: START")
+    # Do with Open AI
+    embeddings = OpenAIEmbeddings()
+    # Do with HuggingFace
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    print("get_vectorstore: END")
+    return vectorstore
+
 
 def main():
     load_dotenv()
@@ -31,8 +58,11 @@ def main():
                 st.write(raw_text)
 
                 # Get Text Chunks
+                text_chunks = get_text_chunks(raw_text)
+                st.write(text_chunks)
 
                 # Create vector store
+                vectorstore = get_vectorstore(text_chunks)
 
 if __name__ == '__main__':
     main()
